f12e4192c9d4ea88a488ddadc67fd9f3a66d77a4
abbas5495 <abbas5495@gmail.com>
2022-09-07 01:25:25 +0530

    abbas's code

--- src/test/java/org/test/project/day2/TrailProject.java
+++ src/test/java/org/test/project/day2/TrailProject.java
@@ -1,5 +1,19 @@
 package org.test.project.day2;
 
 public class TrailProject {
+	public static void main(String[] args) {
+		System.out.println("vol 1");
+		System.out.println("vol 2");
+		System.out.println("vol 3");
+		System.out.println("vol 4");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
--- src/test/java/org/test/projects/day2/TrailProjects.java
+++ src/test/java/org/test/projects/day2/TrailProjects.java
@@ -1,5 +1,12 @@
 package org.test.projects.day2;
 
 public class TrailProjects {
+	public static void main(String[] args) {
+		System.out.println("vol 1");
+		System.out.println("vol 7");
+		System.out.println("vol 8");
+		System.out.println("vol 0");
+		
+	}
 
 }
