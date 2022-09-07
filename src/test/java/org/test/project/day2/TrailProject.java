a1e6db536f9c01879a9fbebe4c23b8537d9dd926
abbas5495 <abbas5495@gmail.com>
2022-09-07 18:50:12 +0530

    sathyamoorthy's code

--- src/test/java/org/test/project/day2/TrailProject.java
+++ src/test/java/org/test/project/day2/TrailProject.java
@@ -1,5 +1,16 @@
 package org.test.project.day2;
 
 public class TrailProject {
+	public static void main(String[] args) {
+		System.out.println("Team 1");
+		System.out.println("Team 2");
+		System.out.println("Team 3");
+		System.out.println("Team 4");
+		
+		
+		
+		
+		
+	}
 
 }
--- src/test/java/org/test/projects/day2/TrailProjects.java
+++ src/test/java/org/test/projects/day2/TrailProjects.java
@@ -1,5 +1,11 @@
 package org.test.projects.day2;
 
 public class TrailProjects {
-
+	public static void main(String[] args) {
+		System.out.println("Team 1");
+		System.out.println("Team 8");
+		System.out.println("Team 5");
+		System.out.println("Team 2");
+		
+	}
 }
